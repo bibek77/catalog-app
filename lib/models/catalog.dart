3ab027d482b469a1c16e79e25f32b4a8b7e88fde
bibek77 <bibekrana01@gmail.com>
2022-02-02 23:33:21 +0530

    list items

--- lib/models/catalog.dart
+++ lib/models/catalog.dart
@@ -1,14 +1,5 @@
 class CatalogModel {
-  static final items = [
-    Item(
-      id : 1,
-      name: "iPhone 13 Pro",
-      desc: "Apple iPhone 13th Generation",
-      price: 999,
-      color: "#33505a",
-      image: "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/iphone-13-pro-family-hero?wid=940&hei=1112&fmt=png-alpha&.v=1631220221000"
-    )
-  ];
+  static List<Item> items = []..length;
 }
 
 class Item {
@@ -19,6 +10,30 @@ class Item {
   final String color;
   final String image;
 
-  Item({required this.id, required this.name, 
-  required this.desc, required this.price, required this.color, required this.image});
+  Item(
+      {required this.id,
+      required this.name,
+      required this.desc,
+      required this.price,
+      required this.color,
+      required this.image});
+
+  factory Item.fromMap(Map<String, dynamic> map) {
+    return Item(
+        id: map["id"],
+        name: map["name"],
+        desc: map["desc"],
+        price: map["price"],
+        color: map["color"],
+        image: map["image"]);
+  }
+
+  toMap() => {
+        "id": id,
+        "name": name,
+        "desc": desc,
+        "price": price,
+        "color": color,
+        "image": image
+      };
 }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,16 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'dart:convert';
 import 'package:flutter_catalog/models/catalog.dart';
 import 'package:flutter_catalog/widgets/drawer.dart';
 import 'package:flutter_catalog/widgets/item_widget.dart';
 
-class Homepage extends StatelessWidget {
-  final int days = 0;
-  final String name = "Catalog App";
+class Homepage extends StatefulWidget {
   const Homepage({Key? key}) : super(key: key);
 
   @override
+  State<Homepage> createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
+  final int days = 0;
+
+  final String name = "Catalog App";
+
+  @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
+
+  loadData() async {
+     await Future.delayed(Duration(seconds: 2));
+    final catalogJson =
+        await rootBundle.loadString("assests/files/catalog.json");
+    final decodeData = jsonDecode(catalogJson);
+    var productsData = decodeData["products"];
+    CatalogModel.items = List.from(productsData)
+        .map<Item>((item) => Item.fromMap(item))
+        .toList();
+        setState(() {
+          
+        });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    final dummyList = List.generate(5, (index) => CatalogModel.items[0]);
+    // final dummyList = List.generate(5, (index) => CatalogModel.items[0]);
     return Scaffold(
       appBar: AppBar(
         title: Text("Catalog App"),
@@ -18,12 +47,15 @@ class Homepage extends StatelessWidget {
       drawer: MyDrawer(),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: ListView.builder(
-          itemCount: dummyList.length,
+        child: (CatalogModel.items != null && CatalogModel.items.isNotEmpty ) 
+        ? ListView.builder(
+          itemCount: CatalogModel.items.length,
           itemBuilder: (context, index) {
-            return ItemWidget(item: dummyList[index]);
+            return ItemWidget(item: CatalogModel.items[index]);
           },
-        ),
+        ) : Center(
+          child: CircularProgressIndicator(),
+        ) ,
       ),
       // Center(
       //     child: Center(
